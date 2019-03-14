7b7b43e80aaef187f99ba7d3f1624a790c0a2364
Suresh <suresh1097@gmail.com>
2019-03-15 00:07:11 +0530

    Included Abstract controller

--- onboarding/src/main/java/com/cts/nw/onboarding/controllers/AbstractController.java
+++ onboarding/src/main/java/com/cts/nw/onboarding/controllers/AbstractController.java
@@ -3,7 +3,13 @@
  */
 package com.cts.nw.onboarding.controllers;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 
 /**
  * @author 656579
@@ -12,6 +18,15 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class AbstractController {
 
-	
+	@InitBinder
+	public void dataBinding(WebDataBinder binder) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		dateFormat.setLenient(false);
+		binder.registerCustomEditor(Date.class, "dateOfBirth", new CustomDateEditor(dateFormat, true));
+		binder.registerCustomEditor(Date.class, "startDate", new CustomDateEditor(dateFormat, true));
+		binder.registerCustomEditor(Date.class, "nationwideIdCreatedDate", new CustomDateEditor(dateFormat, true));
+		binder.registerCustomEditor(Date.class, "fgOnBoardingDate", new CustomDateEditor(dateFormat, true));
+		binder.registerCustomEditor(Date.class, "releaseDate", new CustomDateEditor(dateFormat, true));
+	} 
 	
 }
