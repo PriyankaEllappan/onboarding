55f640fb3d5f1d9307940d3234d9939d98c08e62
Suresh <suresh.baskar2@cognizant.com>
2019-03-21 19:33:53 +0530

    Committing changes for termination Js update

--- onboarding/src/main/webapp/resources/scripts/termination.js
+++ onboarding/src/main/webapp/resources/scripts/termination.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 	};
 	releaseDate.datepicker(options);
 	
-	document.getElementById("defaultOpen").click();
+	document.getElementById("releaseTab").click();
 	loadApprovalStatus();
 	loadBands();
 	loadReleaseStatus();
@@ -53,6 +53,28 @@ function openSpecificTab(evt, tabName) {
     }
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
+}
+
+function openSpecificTabNext(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+           tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+           tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    if(tabName == "projInfo"){
+    	document.getElementById("projTab").className += " active";
+    }
+    if(tabName == "customerInfo"){
+    	document.getElementById("custTab").className += " active";
+    }
+    if(tabName == "releaseInfo"){
+    	document.getElementById("releaseTab").className += " active";
+    }
 }
 
 function loadApprovalStatus(){
