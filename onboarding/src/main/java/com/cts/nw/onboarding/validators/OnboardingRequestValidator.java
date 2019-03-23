f8e18a90815d62bd6f1fc98ab5f39ccc9b55cf72
Suresh <suresh1097@gmail.com>
2019-03-23 15:41:09 +0530

    Minor UI Changes

--- onboarding/src/main/java/com/cts/nw/onboarding/validators/OnboardingRequestValidator.java
+++ onboarding/src/main/java/com/cts/nw/onboarding/validators/OnboardingRequestValidator.java
@@ -3,6 +3,8 @@
  */
 package com.cts.nw.onboarding.validators;
 
+import java.util.regex.Pattern;
+
 import org.springframework.stereotype.Service;
 
 import com.cts.nw.onboarding.bo.EmployeeProjHist;
@@ -16,6 +18,9 @@ public class OnboardingRequestValidator {
 
 	public boolean validate(EmployeeProjHist employeeProjHist) {
 		if(employeeProjHist.getExperience() < 0){
+			return false;
+		}
+		if(!Pattern.matches("[a-zA-Z0-9\\s]*", employeeProjHist.getSkillSet())){
 			return false;
 		}
 		return true;
